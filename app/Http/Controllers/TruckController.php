67a674ad12da2c1afe33f0050da280aa66cc4f63
r0766008 <r0766008@student.thomasmore.be>
2021-01-28 09:14:11 +0100

    Updated statusses upon arrival and departure

--- app/Http/Controllers/TruckController.php
+++ app/Http/Controllers/TruckController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bay;
+use App\Models\Schedule;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Truck;
 
@@ -12,13 +15,39 @@ class TruckController extends Controller
         return Truck::all();
     }
 
-    public function getBayByInfo($info) {
-        $result = Truck::select('license_plate', 'rfid', 'company', 'date', 'eta', 'ata', 'atd', 'number AS bay_number')
+    public function registerTruckOnPremise($info) {
+        $currentDay = Carbon::now();
+        $result = Truck::select('trucks.id', 'license_plate', 'rfid', 'company', 'date', 'eta', 'ata', 'atd', 'number AS bay_number')
             ->where('license_plate', $info)
             ->orWhere('rfid', $info)
             ->join('schedules', 'trucks.id', '=', 'schedules.truck_id')
             ->join('bays', 'schedules.bay_id', '=', 'bays.id')
             ->get();
+        if (Schedule::where([['date', 'like', $currentDay->toDateString()], ['truck_id', 'like', $result[0]->id], ['schedule_status_id', 'like', 1]])->count() > 0) {
+            Schedule::where([['date', 'like', $currentDay->toDateString()], ['truck_id', 'like', $result[0]->id], ['schedule_status_id', 'like', 1]])
+                ->first()
+                ->update(['schedule_status_id' => 2, 'ata' => $currentDay->toTimeString()]);
+        }
+        Bay::where('number', 'like', $result[0]->bay_number)->first()->update(['bay_status_id' => 2]);
+        return response()->json($result);
+    }
+
+    public function registerTruckFinished($info) {
+        $currentDay = Carbon::now();
+        $result = Truck::select('trucks.id', 'license_plate', 'rfid', 'company', 'date', 'eta', 'ata', 'atd', 'number AS bay_number')
+            ->where('license_plate', $info)
+            ->orWhere('rfid', $info)
+            ->join('schedules', 'trucks.id', '=', 'schedules.truck_id')
+            ->join('bays', 'schedules.bay_id', '=', 'bays.id')
+            ->get();
+        if (Schedule::where([['date', 'like', $currentDay->toDateString()], ['truck_id', 'like', $result[0]->id], ['schedule_status_id', 'like', 2]])->count() > 0) {
+            Schedule::where([['date', 'like', $currentDay->toDateString()], ['truck_id', 'like', $result[0]->id], ['schedule_status_id', 'like', 2]])
+                ->first()
+                ->update(['schedule_status_id' => 3, 'atd' => $currentDay->toTimeString()]);
+        }
+        if (Schedule::where([['bay_id', 'like', $result[0]->bay_number], ['schedule_status_id', 'like', 2]])->count() == 0) {
+            Bay::where('number', 'like', $result[0]->bay_number)->first()->update(['bay_status_id' => 1]);
+        }
         return response()->json($result);
     }
 
